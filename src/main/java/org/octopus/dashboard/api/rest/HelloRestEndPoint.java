a770d487fc30f2a6d623de81e84ba403e840df83
ohmylove99 <lengfei9928@hotmail.com>
2016-08-06 07:06:39 +0800

    add rest test

--- src/main/java/org/octopus/dashboard/api/rest/HelloRestEndPoint.java
+++ src/main/java/org/octopus/dashboard/api/rest/HelloRestEndPoint.java
@@ -8,6 +8,8 @@ import org.octopus.dashboard.shared.constants.MediaTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +26,22 @@ public class HelloRestEndPoint {
 	@RequestMapping(value = "/api/rest/hello", method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
 	public Map<String, String> get() {
 		return Collections.singletonMap("hello", "Jason");
+	}
+
+	@RequestMapping(value = "/api/rest/hello/string", method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
+	public String getString() {
+		return "{\"hello\": \"Jason\"}";
+	}
+
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "/api/rest/hello/resmap", method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
+	public ResponseEntity<Map> getStringResEntMap() {
+		return new ResponseEntity<Map>(Collections.singletonMap("hello", "Jason"), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/api/rest/hello/resstring", method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
+	public ResponseEntity<String> getStringResEntString() {
+		return new ResponseEntity<String>("{\"hello\": \"Jason\"}", HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/api/rest/error/{cnt}", method = RequestMethod.GET, produces = MediaTypes.JSON_UTF_8)
